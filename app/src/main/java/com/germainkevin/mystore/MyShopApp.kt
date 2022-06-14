1e5be612feab7ba9d4b0c58bc0aa2a834d48ec4f
germainkevinbusiness <67168053+kevingermainbusiness@users.noreply.github.com>
2022-06-14 15:14:34 -0400

    Turned on R8 for build.gradle

--- app/src/main/java/com/germainkevin/mystore/MyShopApp.kt
+++ app/src/main/java/com/germainkevin/mystore/MyShopApp.kt
@@ -9,6 +9,8 @@ class MyShopApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
